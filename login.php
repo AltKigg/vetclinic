e6188e44429bba38482e3692a5677f73bc918c3a
AltKigg <90957327+AltKigg@users.noreply.github.com>
2023-05-27 00:51:05 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -15,8 +15,7 @@ $dbname="vetclinic";
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 
-  $SELECT = "SELECT Email, Password From doctors Where Email = ? AND Password = ?";
-
+  /*$SELECT = "SELECT Email, Password FROM doctors WHERE Email = ? AND Password = ?";
 //Prepare statement
      $stmt = $con->prepare($SELECT);
      $stmt->bind_param("ss", $email, $pswd);
@@ -26,17 +25,36 @@ $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
      $rnum = $stmt->num_rows;
 
      //checking username
-      if ($rnum==0) 
+      if ($rnum==0)
 	   {
       $error .= '<p class="error">The password is not valid.</p>';
 
      } else {
+        $show = $con->query("SELECT * FROM doctors WHERE (Email = '$email') AND (Password = '$pswd');");
+        $all = $show->fetch_all(MYSQLI_ASSOC);
+        foreach($all as $row) $_SESSION["userid"] = $row['iddoctors'];
 
-        $_SESSION["userid"] = $row['iddoctors'];
-        echo  $_SESSION["userid"];
-        // Redirect the user to welcome page
-        header("location: lk.html");
-     }
-$stmt->close();
+        header("location: lk.phtml");
+     }*/
+
+if($email!="" and $pswd!="")
+{
+    $sql="SELECT * FROM doctors WHERE Email='$email' and Password='$pswd'";
+    $result=$con->query($sql);
+
+    $count=$result->num_rows;
+
+    if($count==1)
+    {
+        $_SESSION['email'] = $email;
+            echo "<h2>". $_SESSION["email"] . "</h2>";
+        header("location:lk.phtml");
+    }
+    else {
+        echo "Ошибка входа";
+    }
+}else {
+    echo "Пожалуйста введите почту и пароль";
+}
 $con->close();
 ?>
